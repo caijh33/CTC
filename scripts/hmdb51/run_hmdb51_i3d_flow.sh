94b3c829fc029f1b9c85fb7e062dd895ad7f189a
caijh33 <57694435+caijh33@users.noreply.github.com>
2020-01-07 21:01:17 +0800

    Add files via upload

---
+++ scripts/hmdb51/run_hmdb51_i3d_flow.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+python train_i3d.py --train_list data/hmdb51/hmdb51_flow_train_split_1.txt \
+--val_list data/hmdb51/hmdb51_flow_val_split_1.txt \
+--dataset hmdb51 \
+--mode flow --arch i3d \
+--save_model checkpoints/ \
+--lr 0.001 --lr_steps 10 20 25 30 35 40 --epochs 45 --batch-size 3 \
+--snapshot_pref checkpoints/ \
+--dropout 0.3
---
+++ scripts/ucf101/run_se_bilinear_i3d_rgb.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+python train_i3d.py --train_list data/ucf101/ucf101_rgb_train_split_1.txt \
+--val_list data/ucf101/ucf101_rgb_val_split_1.txt \
+--dataset ucf101 \
+--mode rgb --arch se_bilinear_i3d \
+--save_model checkpoints/ \
+--lr 0.001 --lr_steps 10 20 25 30 35 40 --epochs 45 --batch-size 3 \
+--snapshot_pref checkpoints/ \
+--dropout 0.3
